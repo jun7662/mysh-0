3c8ef620744b1ea01224ae683e8617a1dbea0cf9
Kim Seungjun <goodkim97@gmail.com>
2017-10-08 21:05:10 +0900

    Add parse_command function

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,27 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+	char *str,*tok;
+	int i = 0;
+
+	str = (char*)malloc(strlen(command)+1);
+	*argv = (char**)malloc(i * sizeof(char**));
+
+	strcpy(str,command);
+	tok = strtok(str," ");
+
+	while(1)
+	{
+		if(tok == NULL) break;
+		*argv = (char**)realloc(*argv, (i+1)*sizeof(char**));
+		*(*argv+i) = (char*)malloc(strlen(tok)+1);
+		strcpy(*(*argv+i++),tok);
+		tok = strtok(NULL," ");
+	}
+	*argc = i;
 }
