1c094f69e26c495fbaaf878ed5601532da1e9046
Kim Seungjun <goodkim97@gmail.com>
2017-10-09 13:27:02 +0900

    Add do functions

--- src/commands.c
+++ src/commands.c
@@ -1,11 +1,13 @@
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+	if(chdir(argv[1])!= 0) return -1;
   // TODO: Fill it!
   return 0;
 }
@@ -13,7 +15,9 @@ int do_cd(int argc, char** argv) {
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
+	char dir[255];
+	if(getcwd(dir,255)==NULL) return -1;
+	printf("%s\n",dir);
   // TODO: Fill it!
 
   return 0;
